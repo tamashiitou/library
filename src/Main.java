4b6d00753dd6f884878a9e5bf00571df0c8fd149
Tamashi Itou <127907733+tamashiitou@users.noreply.github.com>
2024-10-22 18:53:49 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+    Library library = new Library();
+    Scanner sc = new Scanner(System.in); 
     public static void main(String[] args) {
-        
+        showMenu();
+        while(true){
+            String calledFuntionc = sc.nextLine();
+            if(calledFunction==1){
+                lendBook();
+            }
+        }
+    }
+    public static void showMenu(){
+        System.out.println("Please choose a function:");
+        System.out.println("1. Lend books");
+        System.out.println("2. Redeem books");
+        System.out.println("3. Show library information");
+        System.out.println("4. Show customer information");
+    }
+    public static void lendBook(){
+        System.out.println("Type in book information:");
+        System.out.print("Name: ")
     }
 }
+
